a32f593b9155c743da3fdbc522bad1f69d12c784
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-10-27 09:07:00 +0100

    ReaderandWritting finished
    - add exercise r,w with method buffered

--- Ejercicios/ReadingAndWritintTextFilesBuffered/src/ReadWriteTextBufferedReaderWriter.java
+++ Ejercicios/ReadingAndWritintTextFilesBuffered/src/ReadWriteTextBufferedReaderWriter.java
@@ -1,51 +1,62 @@
 import java.io.*;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.Random;
 public class ReadWriteTextBufferedReaderWriter {
-	public static final String inputPath = "C:\\Users\\david\\eclipse-workspace\\ReadingAndWritintTextFilesBuffered\\Racers.txt";
-	public static final String outputPath = "C:\\Users\\david\\eclipse-workspace\\ReadingAndWritintTextFilesBuffered\\RacersModified.txt";
+	public static final String inputPath = "C:\\Users\\dcastilh\\Documents\\GitHub\\Java\\Ejercicios\\ReadingAndWritintTextFilesBuffered\\Racers.txt";
+	public static final String outputPath = "C:\\Users\\dcastilh\\Documents\\GitHub\\Java\\Ejercicios\\ReadingAndWritintTextFilesBuffered\\RacersModified.txt";
 
 	public static void main(String[] args) {
-		File inputFile = new File(inputPath);
-		Scanner input = null;
-		
+		FileReader fr = null;
+		BufferedReader br = null;
 		FileWriter fw = null;
+		BufferedWriter bw = null;
 		
-		ArrayList<String> racers = new ArrayList<String>();
 		try {
-			input = new Scanner(inputFile);
-			while (input.hasNextLine()) {
-				String nextRacer = input.nextLine();
-				racers.add(nextRacer);
-				System.out.println(nextRacer + " added...");
+			fr = new FileReader(inputPath);
+			br = new BufferedReader(fr);
+			String nextLine = null;
+			ArrayList<String> racers = new ArrayList<String>();
+			while ((nextLine= br.readLine()) != null) {
+				racers.add(nextLine);
+				System.out.println("Added " + nextLine);
 			}
-			if (input != null) {
-				input.close();
-				input = null;
-			}
-			
 			fw = new FileWriter(outputPath);
+			bw = new BufferedWriter(fw);
 			Random r = new Random();
 			int max = 85;
 			int min = 18;
+			
 			for (String s : racers) {
 				int age = r.nextInt((max - min)) + min + 1;
 				
-				String output = s + "|" + age + "\n";
-				fw.write(output);
+				String output = s + "|" + age;
+				bw.write(output);
+				bw.newLine();
 			}
 		} catch (IOException ioex) {
 			ioex.printStackTrace();
 		} finally {
-			if (fw != null) {
 				try {
-					fw.close();
-					fw = null;
+					if (fr != null) {
+						if (br != null) {
+							br.close();
+							br = null;
+						}
+						fr.close();
+						fr = null;
+					}
+					if (fw != null) {
+						if (bw != null) {
+							bw.close();
+							bw = null;
+						}
+						fw.close();
+						fw = null;
+					} 
 				} catch (IOException ioex2) {
 					ioex2.printStackTrace();
 				}
 			}
 		}
 	}
-
-}
+ 
