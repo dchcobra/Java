5cadac3755a79d0e0ffd503428c3dd6c8164c612
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-10-30 09:12:44 +0100

    updated

--- Ejercicios/SpringHelloWord/src/HelloWorld/MainApp.java
+++ Ejercicios/SpringHelloWord/src/HelloWorld/MainApp.java
@@ -1,5 +1,9 @@
 package HelloWorld;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 public class MainApp {
 	public static void main(String[] args) {
 		ApplicationContext ctxt = 
