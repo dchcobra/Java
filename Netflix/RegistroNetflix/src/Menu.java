9aedcf40bebf3f245900aac6c609eb9491a28c85
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-11-04 13:30:53 +0100

    Casi terminado la creacion del objeto Pelicula a traves del usuario

--- Netflix/RegistroNetflix/src/Menu.java
+++ Netflix/RegistroNetflix/src/Menu.java
@@ -6,6 +6,11 @@ public class Menu {
 		int ID;
 		String NombreContenido;
 		String Fecha;
+		String valoracion;
+		String ext1;
+		String ext2;
+		int ext3;
+		//String valoracionyf;
 		
 		/* Existen estos tipos
 		contenido = "Pelicula";
@@ -21,17 +26,51 @@ public class Menu {
 		System.out.println(f);
 		*/
 		
-		/*
+		
 		boolean salir = false;
 		while (!salir) {
 			mostrarMenu(); //Muestra las distintas opciones
 			//recogemos el valor de la opcion
 			Scanner lector = new Scanner(System.in);
 			int opcion = lector.nextInt();
+			Scanner lectorID = new Scanner(System.in);
+			Scanner lectorNombre = new Scanner(System.in);
+			Scanner lectorFecha = new Scanner(System.in);
+			Scanner lectorValoracion = new Scanner(System.in);
+			Scanner lectorValoracionyn = new Scanner(System.in);
+			Scanner lectorExt1 = new Scanner(System.in);
+			Scanner lectorExt2 = new Scanner(System.in);
+			Scanner lectorExt3 = new Scanner(System.in);
+
+			
+
 			//Segun el numero que introduzca el usuario, hara una cosa u otra
 			switch (opcion) {
 				case 1: 
-					System.out.println("1");
+					System.out.println("Introduce el ID de la pelicula: ");
+					ID = lectorID.nextInt();
+					System.out.println("Introduce el nombre de la pelicula: ");
+					NombreContenido = lectorNombre.nextLine();
+					System.out.println("Introduce la fecha de reproduccion de la pelicula: ");
+					Fecha = lectorFecha.nextLine();
+					System.out.println("Quieres introducir la valoracion? 1 = si");
+					if (lectorValoracionyn.nextInt() == 1) {
+						System.out.println("Introduce la valoracion: ");
+						valoracion = lectorValoracion.nextLine();
+					} else {
+						valoracion = "Sin valoracion";
+					}
+					System.out.println("Introduce el director de la pelicula: ");
+					ext1 = lectorExt1.nextLine();
+					System.out.println("Introduce el año de publicacion: ");
+					ext3 = lectorExt3.nextInt();
+					
+					System.out.println(ID);
+					System.out.println(NombreContenido);
+					System.out.println(Fecha);
+					System.out.println(valoracion);
+					System.out.println(ext1);
+					System.out.println(ext3);
 					break;
 				case 2:
 					System.out.println("2");
@@ -45,7 +84,7 @@ public class Menu {
 					break;
 			}
 		} 
-		*/ 
+		
 		
 		//Debug de una Pelicula
 		Pelicula p = new Pelicula(1, "Fast&Forious", "2/4/2020", "10", "Paul", 2018);
@@ -61,7 +100,7 @@ public class Menu {
 	
 	//muestra las opciones segun lo que quiera introducir el usuario
 	public static void mostrarMenu() {
-		System.out.println("NETFLIX HISTORIAL");
+		System.out.println("\nNETFLIX HISTORIAL");
 		System.out.println("****************************************");
 		System.out.println("Que contenido quieres indtroducir?\n"
 				+ "1. Guardar historial de una Pelicula\n"
