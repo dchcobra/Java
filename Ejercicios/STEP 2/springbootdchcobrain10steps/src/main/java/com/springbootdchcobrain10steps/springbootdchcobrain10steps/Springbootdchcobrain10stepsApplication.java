0086fc54506fcfea1aea3eed9a915af871a997a4
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-11-13 13:32:33 +0100

    Spring boot actuator

--- Ejercicios/STEP 2/springbootdchcobrain10steps/src/main/java/com/springbootdchcobrain10steps/springbootdchcobrain10steps/Springbootdchcobrain10stepsApplication.java
+++ Ejercicios/STEP 2/springbootdchcobrain10steps/src/main/java/com/springbootdchcobrain10steps/springbootdchcobrain10steps/Springbootdchcobrain10stepsApplication.java
@@ -2,12 +2,18 @@ package com.springbootdchcobrain10steps.springbootdchcobrain10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Springbootdchcobrain10stepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springbootdchcobrain10stepsApplication.class, args);
+		ApplicationContext applicationContext = 
+				SpringApplication.run(Springbootdchcobrain10stepsApplication.class, args);
+		
+		for (String name : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
 	}
 
 }
