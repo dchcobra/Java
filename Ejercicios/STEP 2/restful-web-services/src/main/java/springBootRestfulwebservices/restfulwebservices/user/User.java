1e68a86db88cb2cadb752e8a2d1c61de763e7570
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-11-18 10:41:50 +0100

    Make a ui to see and debug your API

--- Ejercicios/STEP 2/restful-web-services/src/main/java/springBootRestfulwebservices/restfulwebservices/user/User.java
+++ Ejercicios/STEP 2/restful-web-services/src/main/java/springBootRestfulwebservices/restfulwebservices/user/User.java
@@ -8,11 +8,13 @@ import javax.validation.constraints.Size;
 
 
 public class User {
-	// Referencia to javax.validation, you can see on pom.xml 
-	// and use @valid when you create a user
+	/* message = text when return error
+	 Referencia to javax.validation, you can see on pom.xml 
+	 and use @valid when you create a user
+	 */
 	@Size(min=2, message="El nombre ha de tener almenos 2 caracteres")
 	private String name;
-	// Validate date introduce user
+	// Validate date introduce user, can't introduce date future
 	@Past(message="La fecha ha de ser igual o anterior a la actual")
 	private Date birthData;
 	private Integer id;
