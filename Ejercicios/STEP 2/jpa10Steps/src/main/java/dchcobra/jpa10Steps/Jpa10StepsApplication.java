6895f9db7ac455a461618f03064007817b779fde
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-11-18 12:41:53 +0100

    jpa

---
+++ Ejercicios/STEP 2/jpa10Steps/src/main/java/dchcobra/jpa10Steps/Jpa10StepsApplication.java
@@ -0,0 +1,13 @@
+package dchcobra.jpa10Steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa10StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpa10StepsApplication.class, args);
+	}
+
+}
---
+++ Ejercicios/STEP 2/jpa10Steps/src/test/java/dchcobra/jpa10Steps/Jpa10StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package dchcobra.jpa10Steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa10StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
